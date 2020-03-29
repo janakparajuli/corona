08abb8ff8b0374dbbc94e42e171d1b5a9fe8ed42
janakparajuli <janak.parajuli1@gmail.com>
2020-03-29 23:07:41 +0545

    Added refresh button

--- scripts/main.js
+++ scripts/main.js
@@ -34,42 +34,44 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
     });
 
 //Fetching The Case by Country Data 
-fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-            "x-rapidapi-key": "3ada472d03msh2b9f70ac84a7697p1e60abjsn2ef6d2bdfc07"
-        }
-    })
-    .then(response => response.json().then(data => {
-        console.log(data)
-        let countries_stat = data.countries_stat;
-        nep_total_cases.innerHTML = countries_stat[174].cases;
-        nep_new_cases.innerHTML = countries_stat[174].new_cases;
-        nep_new_death.innerHTML = countries_stat[174].new_deaths;
-        nep_total_death.innerHTML = countries_stat[174].deaths;
-        nep_total_recovered.innerHTML = countries_stat[174].total_recovered;
-        // debugger;
-        //Getting all the country statistic using a loop
-        for (let i = 0; i < countries_stat.length; i++) {
-            console.log(countries_stat[i]);
-            //we will start by inserting the new rows inside our table
-            let row = table.insertRow(i + 1);
-            let country_name = row.insertCell(0);
-            let cases = row.insertCell(1);
-            let deaths = row.insertCell(2);
-            let active = row.insertCell(3);
-            let serious_critical = row.insertCell(4);
-            let recovered_per_country = row.insertCell(5);
-            country_name.innerHTML = countries_stat[i].country_name;
-            cases.innerHTML = countries_stat[i].cases;
-            deaths.innerHTML = countries_stat[i].deaths;
-            active.innerHTML = countries_stat[i].active_cases;
-            serious_critical.innerHTML = countries_stat[i].serious_critical;
-            recovered_per_country.innerHTML = countries_stat[i].total_recovered;
+$(document).on("click", "#refresh", function() {
+    fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+                "x-rapidapi-key": "3ada472d03msh2b9f70ac84a7697p1e60abjsn2ef6d2bdfc07"
+            }
+        })
+        .then(response => response.json().then(data => {
+            console.log(data)
+            let countries_stat = data.countries_stat;
+            nep_total_cases.innerHTML = countries_stat[174].cases;
+            nep_new_cases.innerHTML = countries_stat[174].new_cases;
+            nep_new_death.innerHTML = countries_stat[174].new_deaths;
+            nep_total_death.innerHTML = countries_stat[174].deaths;
+            nep_total_recovered.innerHTML = countries_stat[174].total_recovered;
+            // debugger;
+            //Getting all the country statistic using a loop
+            for (let i = 0; i < countries_stat.length; i++) {
+                console.log(countries_stat[i]);
+                //we will start by inserting the new rows inside our table
+                let row = table.insertRow(i + 1);
+                let country_name = row.insertCell(0);
+                let cases = row.insertCell(1);
+                let deaths = row.insertCell(2);
+                let active = row.insertCell(3);
+                let serious_critical = row.insertCell(4);
+                let recovered_per_country = row.insertCell(5);
+                country_name.innerHTML = countries_stat[i].country_name;
+                cases.innerHTML = countries_stat[i].cases;
+                deaths.innerHTML = countries_stat[i].deaths;
+                active.innerHTML = countries_stat[i].active_cases;
+                serious_critical.innerHTML = countries_stat[i].serious_critical;
+                recovered_per_country.innerHTML = countries_stat[i].total_recovered;
 
-        }
-    }))
-    .catch(err => {
-        console.log(err);
-    });
+            }
+        }))
+        .catch(err => {
+            console.log(err);
+        })
+});
